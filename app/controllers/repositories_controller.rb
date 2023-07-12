b0f88214dcbc9924304214e699c7ccbdd468b71e
Kapil Patel <kapilppatel03@gmail.com>
2023-07-12 16:42:46 +0530

    minor changes

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,16 +1,23 @@
 class RepositoriesController < ApplicationController
 
-  def search
+  def show
     repo_hash = ScrapperService.new(params[:username]).call
     create_repo(repo_hash)
+  end
+  
+  def search
   end
 
   def create_repo(result)
     if !result.include?(:message)
       result.each do |data|
-        repo = user.repositories.find_or_create_by(title: data[:name])
+        repo = user.repositories.find_or_create_by(title: data[:title])
+        debugger
         repo.star_count = data[:stars]
+        repo.language = data[:language]
+        repo.url = data[:url]
         repo.save!
+        @repo = repo
       end
     else
       return result[:message]
